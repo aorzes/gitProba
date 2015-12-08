d38fd996be7156fe4f3435d093683d1b7f7c4fc2
aorzes <anton.orzes@gmail.com>
2015-12-08 20:54:13 +0100

    Initial Commit

---
+++ gitProba01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitProba01
+//
+//  Created by Anton Orzes on 08.12.2015..
+//  Copyright © 2015. Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitProba01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitProba01
+//
+//  Created by Anton Orzes on 08.12.2015..
+//  Copyright © 2015. Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
